9af291a8f521db2153eba7328b42b8d6e3801d3a
七彩 <wangriqiao@cai-inc.com>
2025-09-30 17:59:00 +0800

    feat: 去除无用代码

--- netlify/functions/hello.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  console.log(event, context, '---参数是啥？');
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello, World!' }),
-  };
-};
